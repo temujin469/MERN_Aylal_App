284dff9ad1133adc5688336bb5775e9080e95a47
aylalwebsite <ttemujin469@gmail.com>
2022-04-23 20:38:58 +0800

    second commit

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,22 @@ const dotenv = require('dotenv')
 const pinRoute = require('./route/pins')
 const userRoute = require('./route/users')
 const cors = require('cors')
+const path = require('path')
 
 dotenv.config();
+
+
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true})
-.then(()=>{console.log('mongoDB has connected***')})
-.catch(err=>console.log(`ERROR_IS: ${err}`))
 
 app.use("/api/pins",pinRoute)
 app.use("/api/users",userRoute)
+
+app.get("/",(req,res)=>{
+    res.send('kdffkds')
+})
 
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
