fd87d9214e3df8f0265d9f05520ddfe2d6e01789
temujin469 <ttemujin469@gmail.com>
2022-12-01 23:16:16 +0800

    hahahaha

--- backend/frontend/src/config.js
+++ backend/frontend/src/config.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL :"https://aylal-website.herokuapp.com/api/"
-})
+  baseURL: process.env.BASE_URL,
+});
