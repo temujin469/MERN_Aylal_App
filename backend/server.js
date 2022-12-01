48767536af72917b77e17651cf02efb86bb587bf
temujin469 <ttemujin469@gmail.com>
2022-12-01 14:45:06 +0800

    zaswarad

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,32 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const pinRoute = require('./route/pins')
-const userRoute = require('./route/users')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const pinRoute = require("./route/pins");
+const userRoute = require("./route/users");
+const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
-
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true})
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
-app.use("/api/pins",pinRoute)
-app.use("/api/users",userRoute)
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
-app.get("/",(req,res)=>{
-    res.send('kdffkds')
-})
-
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+app.get("/", (req, res) => {
+  res.send("kdffkds");
 });
 
-const port = process.env.PORT || 5000
+app.use(express.static(path.resolve(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/frontend/build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port);
